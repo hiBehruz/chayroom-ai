270becd55df686cd704d920301342f77d4c18cbf
Behruz <ibehruzzaripov@gmail.com>
2026-06-30 14:29:37 +0500

    ci: reduce integration test worker memory

--- vitest.config.ts
+++ vitest.config.ts
@@ -31,6 +31,8 @@ export default defineConfig({
           include: ['tests/integration/**/*.test.ts'],
           testTimeout: 60_000,
           hookTimeout: 60_000,
+          maxWorkers: 1,
+          isolate: false,
           fileParallelism: false
         }
       }
